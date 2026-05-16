9984db4fd6ec8a02e127a904637390a1de95b435
punnu143 <khushiprjapati4449@gmail.com>
2026-05-16 23:32:06 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -262,4 +262,12 @@ def main(message):
 
 print("🔥 Dark Lord Fully Loaded...")
 
-bot.infinity_polling()
+while True:
+    try:
+        bot.infinity_polling(
+            timeout=30,
+            long_polling_timeout=30
+        )
+    except Exception as e:
+        print("Polling Error:", e)
+        time.sleep(5)
